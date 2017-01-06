ca97680754bfc4b0fdec18b8c5cd82f920e12873
lekimdung <kidule123@gmail.com>
2017-01-06 23:25:15 +0700

    dung commit

---
+++ Models/DeleteHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBanBanh.Models
+{
+    public class DeleteHelper
+    {
+        dbQLBanBanhDataContext db = new dbQLBanBanhDataContext();
+        public void DeleteChiTietTheoDDH(int maDDH)
+        {
+            var chiTiets = db.CHITIETDDHs.Where(m => m.MaDDH == maDDH).ToList();
+            foreach (var item in chiTiets)
+            {
+                db.CHITIETDDHs.DeleteOnSubmit(item);
+            }
+            db.SubmitChanges();
+        }
+        public void DeleteChiTietTheoSP(int maSP)
+        {
+            var chiTiets = db.CHITIETDDHs.Where(d => d.MaSP == maSP);
+            foreach (var item in chiTiets)
+            {
+                db.CHITIETDDHs.DeleteOnSubmit(item);
+            }
+            db.SubmitChanges();
+        }
+        public void DeleteDonDatHang(int maDDH)
+        {
+            DeleteChiTietTheoDDH(maDDH);
+            var donDatHang = db.DONDATHANGs.FirstOrDefault(d => d.MaDDH == maDDH);
+            db.DONDATHANGs.DeleteOnSubmit(donDatHang);
+            db.SubmitChanges();
+        }
+        public void DeleteSanPham(int maSP)
+        {
+            DeleteChiTietTheoSP(maSP);
+            var sanPhams = db.SANPHAMs.FirstOrDefault(m => m.MaSP == maSP);
+            db.SANPHAMs.DeleteOnSubmit(sanPhams);
+            db.SubmitChanges();
+        }
+    }
+}
---
+++ Models/GioHang.cs
@@ -0,0 +1,45 @@
+﻿using WebBanBanh.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+
+namespace WebBanBanh.Models
+{
+    public class GioHang
+    {
+        dbQLBanBanhDataContext data = new dbQLBanBanhDataContext();
+        public int iMaSP { set; get; }
+        public string sTenSP { set; get; }
+        public string sAnhBia { set; get; }
+        public Double dGiaBan { set; get; }
+        public int sSoLuong { set; get; }
+        public Double dThanhTien { get { return sSoLuong * dGiaBan; } }
+        public GioHang(int Masp)
+        {
+            iMaSP = Masp;
+            SANPHAM sp = data.SANPHAMs.Single(n => n.MaSP == iMaSP);
+            sTenSP = sp.TenSP;
+            sAnhBia = sp.AnhBia;
+            dGiaBan = double.Parse(sp.GiaBan.ToString());
+            sSoLuong = 1;
+        }
+    }
+    public class NguoiNhan
+    {
+        [Required(ErrorMessage = "Vui lòng nhập tên người nhận")]
+        public string TenNguoiNhan { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập sô điện thoại người nhận")]
+        public string SdtNguoiNhan { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập địa chỉ giao hàng")]
+        public string DCGiaoHang { get; set; }
+
+        public DateTime NgayGiao { get; set; }
+    }
+}
